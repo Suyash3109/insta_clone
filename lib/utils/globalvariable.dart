2e23f897ae0a6465859f0e2369c4759875ea7557
top_dawg <tripathisuyash00@gmail.com>
2022-07-01 12:11:53 +0530

    second push

--- lib/utils/globalvariable.dart
+++ lib/utils/globalvariable.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:insta_clone/screens/add_post.dart';
+import 'package:insta_clone/screens/feed_screen.dart';
 
 const webScreenSize = 600;
 
 const homescreenitems = [
-  Text('home'),
+  FeedScreen(),
   Text('search'),
   addpost(),
   Text('fav'),
